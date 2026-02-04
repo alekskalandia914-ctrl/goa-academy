30c2a5e0388ff461963b3137401ac6de04b6c7c6
alekskalandia914-ctrl <alekskalandia914@gmail.com>
2026-02-04 21:26:55 +0400

    day 1

---
+++ level 1/davaleba1.py
@@ -0,0 +1,16 @@
+name = "Alex"
+age = 14
+address = "Tbilisi, Georgia"
+height = 170
+weight = 65
+
+print(name)
+print( age)
+print(address)
+print(height,)
+print(weight)
+
+
+print("Alex")  # string ფრჩხილებში ჩასმული სიმბოლოები
+print("age", 14) # integer ნებისმიერი ჩასმული მთელი რიცხვი 
+print("ragaca" , 17.1 ) # float ნებისმიერი არა მთელი რიცხვი
