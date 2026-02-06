ca6c8fcbeca3b774981f0e17017b48607fa52430
alekskalandia914-ctrl <alekskalandia914@gmail.com>
2026-02-06 21:18:56 +0400

    day 2

---
+++ level 2/mine.py
@@ -0,0 +1,29 @@
+number1 = 15
+number2 = 25
+print(number1 * number2)
+print(number1 / number2)
+print(number1 ** number2)
+print(number1 // number2)
+print(number1 % number2)
+
+birth_year = 2011
+current_year = 2026
+print(current_year - birth_year , ("wlis xar"))
+
+numberr1 = 14
+numberr2 = 54
+number3 = 123 
+number4 = 444
+number5 = 112
+pasuxi = (numberr1+numberr2+number3+number4+number5)/5
+print(pasuxi)
+
+#დეკლარაცია - არის ცვლადის შექმნა ,მე ვეუბნები პროგრამას რომ x = 10 , შეიქმნა ცვლადი
+#ინიციალიზება - არის ცვლადისთვის პირველი მნიშვნელობის მინიჭება x = 333
+#რეინიციალიზება - მე შემიძლია როგორც მივანიჭო პირველი მნიშვნელობა ასევე შევცვალო სხვა მნიშვნელობად მაგ: x=10, x=25
+#კონკატენაცია - ჩვენ შეგვიდზლია შევაერთოთ ორი მნიშვნელობა იმისთვის რომ მივიღოთ ამ 2 მნიშვნელობისგან ახალი მნიშვნელობა, მაგ: x = მიყვარს , y = გოა , result = x + " " + y და გამოვა მიყვარს გოა
+name = "Tom"
+level = 14
+print(name)
+level = level +1
+print(level)
