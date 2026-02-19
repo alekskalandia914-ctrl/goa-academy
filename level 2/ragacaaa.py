1c06a75fdc12181b11de83e833c60eb2dbd311ee
alekskalandia914-ctrl <alekskalandia914@gmail.com>
2026-02-19 20:47:24 +0400

    day 5

---
+++ level 2/ragacaaa.py
@@ -0,0 +1,11 @@
+user_first_name = "Alex"
+user_last_name = "Kalandia"
+user_age = 20
+user_home_address = "Tbilisi, Georgia"
+user_hobby_activity = "Programming"
+
+print(user_first_name)
+print(user_last_name)
+print(user_age)
+print(user_home_address)
+print(user_hobby_activity)
