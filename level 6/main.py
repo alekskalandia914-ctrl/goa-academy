696794e8750c92b4fbf55af0dce30fae0f9f43b6
alekskalandia914-ctrl <alekskalandia914@gmail.com>
2026-02-20 23:18:18 +0400

    day 6

---
+++ level 6/main.py
@@ -0,0 +1,33 @@
+#პროგრამირებაში არის ამდენი ცვლადი რადგან კომპიტერმა იცოდეს რა სახის ინფრმაცია ინახება მასში
+#მაგალით ცხოვრებიდან: input-ბარათის ჩადება ბანკომატში და პინ კოდის ჩაწერა , output: ფულის მიღება
+name = "Alex"   
+age = 14
+height = 1.72
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+
+km = float(input("შეიყვანე მანძილი კილომეტრებში: "))
+
+meters = km * 1000
+
+print("მანძილი მეტრებში არის:", meters)
+
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+
+print("ჯამი:", num1 + num2)
+print("სხვაობა:", num1 - num2)
+print("ნამრავლი:", num1 * num2)
+print("გამყოფი:", num1 / num2)
+
+
+weight = float(input("შეიყვანე შენი წონა: "))
+height = float(input("შეიყვანე შენი სიმაღლე: "))
+
+bmi = weight / (height * height)
+
+print("შენი BMI არის:", bmi)
